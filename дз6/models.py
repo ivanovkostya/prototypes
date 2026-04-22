2ed1b94382d8d7200453ee08840635635ee2351d
Ivanov Konstantin <ivanov.k@citilink.ru>
2026-04-22 19:12:31 +0300

    добавлено дз6

---
+++ дз6/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True)
+    last_name = Column(String)
+    first_name = Column(String)
+    faculty = Column(String)
+    course = Column(String)
+    grade = Column(Integer)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
